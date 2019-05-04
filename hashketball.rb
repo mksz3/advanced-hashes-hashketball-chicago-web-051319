472e09fbaeaa5ce76c41e9044bf60a9d0fdd41a0
Markus Sanders <markus.sanderst@gmail.com>
2019-05-04 19:18:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,7 @@ def team_colors(team)
 end
 
 def team_names
-  [game_hash[:home][:team_names], game_hash[:away][:team_names]]
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
 end
 
 
